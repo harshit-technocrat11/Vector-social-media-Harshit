b1cd1d9476345fd5f421950e928e0485e4f2f4a9
Rishika Purbey <rishikapurbey712@gmail.com>
2026-06-07 21:49:02 +0530

    fix: add rate limiting to notification and review write endpoints

--- server/src/routes/notification.routes.js
+++ server/src/routes/notification.routes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import auth from "../middlewares/auth.middleware.js";
-import {deleteAllNotifications, deleteMultipleNotifications, deleteNotification, getNotifications, markAllAsRead, markAsRead } from "../controllers/notification.controller.js";
+import { deleteAllNotifications, deleteMultipleNotifications, deleteNotification, getNotifications, markAllAsRead, markAsRead } from "../controllers/notification.controller.js";
+import { commentWriteLimiter } from "../middlewares/rateLimit.middleware.js";
 
 const notificationRouter = express.Router();
 
 notificationRouter.get("/", auth, getNotifications);
-notificationRouter.put("/read-all", auth, markAllAsRead);
-notificationRouter.delete("/all", auth, deleteAllNotifications);
+notificationRouter.put("/read-all", auth, commentWriteLimiter, markAllAsRead);
+notificationRouter.delete("/all", auth, commentWriteLimiter, deleteAllNotifications);
 notificationRouter.put("/:id/read", auth, markAsRead);
 notificationRouter.delete("/:id", auth, deleteNotification);
-notificationRouter.post("/bulk-delete", auth, deleteMultipleNotifications);
+notificationRouter.post("/bulk-delete", auth, commentWriteLimiter, deleteMultipleNotifications);
 
 export default notificationRouter;
--- server/src/routes/review.routes.js
+++ server/src/routes/review.routes.js
@@ -5,10 +5,11 @@ import {
   getReviews,
   getAverage,
 } from "../controllers/review.controller.js";
+import { postWriteLimiter } from "../middlewares/rateLimit.middleware.js";
 
 const router = express.Router();
 
-router.post("/", authMiddleware, createReview);
+router.post("/", authMiddleware, postWriteLimiter, createReview);
 router.get("/", getReviews);
 router.get("/average", getAverage);
 
